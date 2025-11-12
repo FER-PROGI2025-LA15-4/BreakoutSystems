97c7053f3d3c28dd9a8be174bdc59e159c468901
Aurora Necko <aurora.necko@fer.unizg.hr>
2025-11-12 19:12:57 +0100

    Gotov css za desktop i mobile verziju pocetne stranice, #32 #12

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -4,13 +4,17 @@ import fer_logo from "../assets/images/FER-logo.svg";
 
 function Footer() {
     return (
-        <footer className="footer">
-            <img src={logo} alt="Logo BreakoutSystems platforme" className="footer-logo-img"/>
-            <div className="footer-msg">
-                <p>Ova platforma izrađena je u sklopu projekta iz kolegija Programsko inženjerstvo na Fakultetu elektrotehnike i računarstva.</p>
+        <footer>
+            <div className="footer">
+                <img src={logo} alt="Logo BreakoutSystems platforme" className="footer-logo-img"/>
+                <div className="footer-msg">
+                    <p>Ova platforma izrađena je u sklopu projekta iz kolegija Programsko inženjerstvo na Fakultetu elektrotehnike i računarstva.</p>
+                </div>
+                <img src={fer_logo} alt="Logo Fakulteta elektrotehnike i računarstva, Sveučilište u Zagrebu" className="footer-fer-logo-img"/>
+            </div>
+            <div className="copyright">
                 <p>&copy; 2025. BreakoutSystems (ekipa TG15.4)</p>
             </div>
-            <img src={fer_logo} alt="Logo Fakulteta elektrotehnike i računarstva, Sveučilište u Zagrebu" className="footer-fer-logo-img"/>
         </footer>
     );
 }
