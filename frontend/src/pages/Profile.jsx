50df800e8a506ed68773ac853f1d29cb4bc631e0
Antonio Mamić <antoniomamic007@gmail.com>
2025-11-12 22:35:59 +0100

    add register form #35

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,12 +1,15 @@
 import React from "react";
+import { useState } from "react";
 import PageTemplate from "./PageTemplate";
 import registerLogo from "../assets/icons/address-card.svg"
 import loginLogo from "../assets/icons/sign-in.svg"
 import githubLogo from "../assets/icons/github-logo.svg"
+import registerImage from "../assets/icons/user-settings.svg"
 
 function ProfilePage() {
     const name = "profile";
-    const body = (
+
+    const profileLoginBody = (
         <div className="login-background">
             <div className="login-container">
                 <img src={loginLogo} alt="login logo"/>
@@ -25,6 +28,106 @@ function ProfilePage() {
             </div>
         </div>
     );
+
+    const [selectedOption, setSelectedOption] = useState("");
+    const handleRadioChange = (event) => {
+        setSelectedOption(event.target.value);
+    };
+    const profileRegisterBody = (
+        <div className="login-background">
+            <div className="login-container register-form-container">
+                <img src={registerImage} alt="login logo"/>
+                <h2>Registracija</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+
+                <form action={"/auth/register"} method="POST" encType={"multipart/form-data"} className="register-form">
+                    <div className={"register-form-element"}>
+                        <label htmlFor="username">Korisničko ime</label><br/>
+                        <input type="text" id="username" name="username" required={true}/>
+                    </div>
+
+                    <div className={"register-form-element"}>
+                        <p>Uloga</p><br/>
+                        <label>
+                            <input
+                                name="uloga"
+                                type="radio"
+                                value="POLAZNIK"
+                                checked={selectedOption === 'POLAZNIK'}
+                                onChange={handleRadioChange}
+                            />
+                            Igrač
+                        </label><br/>
+                        <label>
+                            <input
+                                name="uloga"
+                                type="radio"
+                                value="VLASNIK"
+                                checked={selectedOption === 'VLASNIK'}
+                                onChange={handleRadioChange}
+                            />
+                            Vlasnik
+                        </label>
+                    </div>
+
+                    {selectedOption === 'POLAZNIK' && (
+                        <div className={"register-form-element"}>
+                            <div>
+                                <label htmlFor="email">Email</label><br/>
+                                <input type="email" id="email" name="email" required={true}/>
+                            </div>
+                            <div>
+                                <label htmlFor="image">Slika profila</label><br/>
+                                <input type="file" id="image" name="image" accept="image/*"/>
+                            </div>
+                        </div>
+                    )}
+
+                    {selectedOption === 'VLASNIK' && (
+                        <div className={"register-form-element"}>
+                            <div>
+                                <label htmlFor={"naziv_tvrtke"}>Naziv tvrtke</label><br/>
+                                <input type="text" id={"naziv_tvrtke"} name={"naziv_tvrtke"} required={true} />
+                            </div>
+
+                            <div>
+                                <label htmlFor="image">Logo tvrtke</label><br/>
+                                <input type="file" id="image" name="image" accept="image/*"/>
+                            </div>
+
+                            <div className={"register-form-owner-address"}>
+                                <div>
+                                    <label htmlFor={"adresa"}>Adresa tvrtke</label><br/>
+                                    <input type="text" id={"adresa"} name={"adresa"} required={true}/>
+                                </div>
+                                <div>
+                                    <label htmlFor={"grad"}>Grad</label><br/>
+                                    <input type="text" id={"grad"} name={"grad"} required={true}/>
+                                </div>
+                            </div>
+
+                            <div>
+                                <label htmlFor={"telefon"}>Telefonski broj tvrtke </label><br/>
+                                <input type="text" id={"telefon"} name={"telefon"} required={true}/>
+                            </div>
+                        </div>
+                    )}
+
+                    <button type={"submit"}>
+                        <img src={githubLogo} alt="github logo"/>
+                        <p>REGISTRIRAJ SE!</p>
+                    </button>
+                </form>
+            </div>
+        </div>
+    );
+
+    let body;
+    if ("korisnik prijavljen") {
+        body = profileRegisterBody;
+    } else {
+        body = profileLoginBody;
+    }
     /*
     const body = <div>
         <a href="/auth/login">
