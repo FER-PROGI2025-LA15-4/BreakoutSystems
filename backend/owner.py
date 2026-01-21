f80a5285e6db9de7d35997c64cde9138c5b6f75d
Filip Šušak <filip.suusak@gmail.com>
2026-01-21 18:00:58 +0100

    #122 dodavanje termina

--- backend/owner.py
+++ backend/owner.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, jsonify, request
 from flask_login import current_user,login_required
 from auth import get_db_connection
+from datetime import datetime, timedelta,timezone
+
 
 owner_bp = Blueprint('owner', __name__)
 
@@ -122,3 +124,46 @@ def get_owner_team_info():
 
     db.close()
     return jsonify(result), 200
+
+@owner_bp.route('/api/owner/add-appointment', methods=['POST'])
+@login_required
+def add_appointment():
+    if current_user.uloga != "VLASNIK":
+        return jsonify({'error': 'forbidden access'}), 403
+
+    data = request.get_json() or {}
+    room_id = data.get("room_id")
+    date_time_string = data.get("dt")
+
+    if not room_id or not date_time_string:
+        return jsonify({'error': 'Missing data'}), 400
+
+    db = get_db_connection()
+    try:
+        room = db.execute(
+            "SELECT 1 FROM EscapeRoom WHERE room_id = ? AND vlasnik_username = ?",
+            (room_id, current_user.username)
+        ).fetchone()
+
+        if not room:
+            return jsonify({'error': 'forbidden access'}), 403
+
+        #Parsiranje vremena (ISO format sa 'Z' na kraju)
+        # Zamjenjujemo 'Z' sa '+00:00' da bi fromisoformat radio ispravno na starijim Python verzijama
+        # ili koristimo UTC timezone
+        dt_object = datetime.fromisoformat(date_time_string.replace('Z', '+00:00'))
+
+        if dt_object < datetime.now(timezone.utc):
+            return jsonify({'error': 'Cannot add appointment in the past'}), 400
+
+        db.execute(
+            "INSERT INTO Termin (room_id, datVrPoc) VALUES (?, ?)",
+            (room_id, date_time_string)
+        )
+        db.commit()
+        return jsonify({"success": True}), 200
+    except Exception as e:
+        db.rollback()
+        return jsonify({'error': str(e)}), 403
+    finally:
+        db.close()
