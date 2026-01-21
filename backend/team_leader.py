efab05e0ef49ca849b95f9f793289430911ce62f
Filip Šušak <filip.suusak@gmail.com>
2026-01-21 16:15:09 +0100

    #114 i #113 rijeseni, i prepravljena my-teams funckcija

--- backend/team_leader.py
+++ backend/team_leader.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, jsonify, request
 from flask_login import current_user,login_required
-from auth import get_db_connection
+from auth import get_db_connection, save_temp_file, move_temp_image, delete_image_file
 
 leader_bp = Blueprint('leader', __name__)
 
@@ -48,3 +48,82 @@ def add_member():
     db.commit()
     db.close()
     return jsonify({"status": "invite_created"}), 200
+
+@leader_bp.route('/api/create-team', methods=['POST'])
+@login_required
+def create_team():
+
+    #name - ime tima
+    #image - slika tima
+
+    if current_user.uloga != 'POLAZNIK':
+        return jsonify({'error': 'forbidden access'}), 403
+
+    team_name = request.form.get("name")
+    image_file = request.files.get("image")
+
+    if not team_name:
+        return jsonify({'error': 'team name missing'}), 400
+
+    db = get_db_connection()
+
+    name_is_taken = db.execute('SELECT 1 FROM Tim WHERE ime = ?', (team_name,)).fetchone()
+
+    if name_is_taken:
+        db.close()
+        return jsonify({'error': 'Team name already taken!'}), 400
+
+
+    image_url = None
+    if image_file and image_file.filename:
+        temp_filename = save_temp_file(image_file)
+        image_url = move_temp_image(temp_filename)
+
+    db.execute('INSERT INTO Tim (ime, image_url, voditelj_username) VALUES (?,?,?)', (team_name, image_url, current_user.username),)
+
+    db.commit()
+    db.close()
+    print("Stvoren tim")
+
+    return jsonify({'status': 'Team created'}), 200
+
+@leader_bp.route('/api/edit-team', methods=['POST'])
+@login_required
+def edit_team():
+    # name - ime tima
+    # image - slika tima
+
+    if current_user.uloga != 'POLAZNIK':
+        return jsonify({'error': 'forbidden access'}), 403
+
+    team_name = request.form.get("name")
+    image_file = request.files.get("image")
+
+    if not team_name:
+        return jsonify({'error': 'team name missing'}), 400
+
+    db = get_db_connection()
+
+    team = db.execute('SELECT image_url FROM Tim WHERE ime = ? AND voditelj_username = ?', (team_name, current_user.username,)).fetchone()
+
+    if team is None:
+        db.close()
+        return jsonify({'error': 'forbidden access'}), 403
+
+
+    if image_file and image_file.filename:
+        ##Brisanje stare slike ako dodajemo novu za stednju memorije - salje se ime fajla za brisanje
+        if team['image_url']:
+            delete_image_file(team['image_url'].split('/')[-1])
+        temp_filename = save_temp_file(image_file)
+        image_url = move_temp_image(temp_filename)
+
+        db.execute('UPDATE Tim SET image_url = ? WHERE ime = ?',
+                   (image_url, team_name),)
+
+    db.commit()
+    db.close()
+
+    return jsonify({'status': 'Team data edited'}), 200
+
+
