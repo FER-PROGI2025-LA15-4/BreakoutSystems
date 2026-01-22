71b77348ffef66b55ef7ce8dee265936eaeb8689
Aurora Necko <aurora.necko@fer.unizg.hr>
2026-01-22 19:28:53 +0100

    popravci pri testiranju

--- backend/player.py
+++ backend/player.py
@@ -1,10 +1,12 @@
 from flask import Blueprint, jsonify, request
-from flask_login import current_user,login_required
+from flask_login import current_user, login_required
 from db_connection import get_db_connection
 
 player_bp = Blueprint('player', __name__)
 
 # dohvat povijesti igara jednog polaznika
+
+
 @player_bp.route('/api/game-history', methods=['GET'])
 @login_required
 def get_game_history():
@@ -14,24 +16,27 @@ def get_game_history():
     db = get_db_connection()
     rows = db.execute("""SELECT naziv, c.room_id, c.datVrPoc, t.ime_tima
                             FROM ClanNaTerminu c JOIN Termin t ON c.room_id = t.room_id AND c.datVrPoc = t.datVrPoc
-                            JOIN EscapeRoom e ON e.room_id = t.room_id""").fetchall()
+                            JOIN EscapeRoom e ON e.room_id = t.room_id WHERE c.username = ?""", (current_user.username,)).fetchall()
 
     history = []
     for row in rows:
         room_id = row["room_id"]
-        ocjena = db.execute("SELECT vrijednost_ocjene FROM OcjenaTezine WHERE room_id = ? AND username = ?", (room_id, current_user.username)).fetchone()
+        ocjena = db.execute("SELECT vrijednost_ocjene FROM OcjenaTezine WHERE room_id = ? AND username = ?",
+                            (room_id, current_user.username)).fetchone()
         history.append({
             "room_name": row["naziv"],
             "room_id": room_id,
             "termin": row["datVrPoc"],
             "ime_tima": row["ime_tima"],
-            "ocjena_tezine": ocjena
+            "ocjena_tezine": ocjena["vrijednost_ocjene"] if ocjena else None
         })
 
     db.close()
     return jsonify({"history": history}), 200
 
 # API preko kojeg korisnik ocjenjuje neku sobu
+
+
 @player_bp.route('/api/rate-room', methods=['POST'])
 @login_required
 def rate_room():
@@ -43,20 +48,24 @@ def rate_room():
     rating = data.get("rating")
 
     db = get_db_connection()
-    played = db.execute("SELECT * FROM ClanNaTerminu WHERE room_id = ? AND username = ?", (room_id, current_user.username)).fetchone()
+    played = db.execute("SELECT * FROM ClanNaTerminu WHERE room_id = ? AND username = ?",
+                        (room_id, current_user.username)).fetchone()
     if played is None:
         db.close()
         return jsonify({'error': 'forbidden access'}), 403
 
-    rtg_exists = db.execute("SELECT * FROM OcjenaTezine WHERE room_id = ? AND username = ?", (room_id, current_user.username)).fetchone()
+    rtg_exists = db.execute("SELECT * FROM OcjenaTezine WHERE room_id = ? AND username = ?",
+                            (room_id, current_user.username)).fetchone()
 
     cursor = db.cursor()
 
     if rtg_exists is None:
-        cursor.execute("INSERT INTO OcjenaTezine VALUES (room_id, username, vrijednost_ocjene) VALUES (?,?,?)", (room_id, current_user.username, rating))
+        cursor.execute("INSERT INTO OcjenaTezine VALUES (room_id, username, vrijednost_ocjene) VALUES (?,?,?)",
+                       (room_id, current_user.username, rating))
 
     else:
-        cursor.execute("UPDATE OcjenaTezine SET vrijednost_ocjene = ? WHERE room_id = ? AND username = ?", (rating, room_id, current_user.username))
+        cursor.execute("UPDATE OcjenaTezine SET vrijednost_ocjene = ? WHERE room_id = ? AND username = ?",
+                       (rating, room_id, current_user.username))
 
     db.commit()
     db.close()
@@ -64,6 +73,8 @@ def rate_room():
     return jsonify({"success": True}), 200
 
 # dohvat timova jednog polaznika
+
+
 @player_bp.route('/api/my-teams', methods=['GET'])
 @login_required
 def get_my_teams():
@@ -83,7 +94,8 @@ def get_my_teams():
     result = []
 
     for team in teams:
-        members = db.execute("SELECT username FROM ClanTima WHERE ime_tima = ?", (team["ime"],)).fetchall()
+        members = db.execute(
+            "SELECT username FROM ClanTima WHERE ime_tima = ?", (team["ime"],)).fetchall()
 
         result.append({
             "name": team["ime"],
@@ -108,7 +120,8 @@ def update_invite():
     invite_update = data.get("invite_update")
 
     db = get_db_connection()
-    invite = db.execute("SELECT accepted FROM ClanTima WHERE ime_tima = ? AND username = ?", (team_name,current_user.username,)).fetchone()
+    invite = db.execute("SELECT accepted FROM ClanTima WHERE ime_tima = ? AND username = ?",
+                        (team_name, current_user.username,)).fetchone()
 
     if invite is None:
         db.close()
@@ -143,11 +156,13 @@ def get_my_invites():
     if current_user.uloga != "POLAZNIK":
         return jsonify({'error': 'forbidden access'}), 403
     db = get_db_connection()
-    invites = db.execute("SELECT * FROM ClanTima WHERE username = ? AND accepted = 0", (current_user.username,)).fetchall()
+    invites = db.execute(
+        "SELECT * FROM ClanTima WHERE username = ? AND accepted = 0", (current_user.username,)).fetchall()
     result = []
     for invite in invites:
         team_name = invite["ime_tima"]
-        team = db.execute("SELECT * FROM Tim WHERE ime_tima = ?", (team_name,)).fetchone()
+        team = db.execute(
+            "SELECT * FROM Tim WHERE ime_tima = ?", (team_name,)).fetchone()
         result.append({
             "name": team["ime"],
             "logo": team["image_url"],
